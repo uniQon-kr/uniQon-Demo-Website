7167ff3ad84e043a4361a0134cf64de6e2280f09
Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
2020-09-17 08:53:04 -0500

    Template Updated

--- assets/js/loginCheck.js
+++ assets/js/loginCheck.js
@@ -1,5 +1,6 @@
-// TODO: Check whether signedIn value in localStorage has been set
+if (localStorage.getItem("uniQonSignedIn") === null) {
     // If not setted, call GET https://api.uniqon.kr/auth to check whether user is logged in or not
-        // Save signedIn value to local Storage
+    // Save signedIn value to local Storage
+}
 // TODO: parse localStorage and 
 // display button accordingly
