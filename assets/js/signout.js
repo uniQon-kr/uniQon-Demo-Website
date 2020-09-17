5398b9bd16b12de9d6289742a308b00c09d9ade3
hse2527 <55567478+hse2527@users.noreply.github.com>
2020-09-18 02:21:45 +0900

    signout fix

--- assets/js/signout.js
+++ assets/js/signout.js
@@ -1,11 +1,14 @@
 
 async function signOutFunc(){
-  const response = await fetch("http://api.uniqon.kr/auth/signout",{
+  const response = await fetch("https://api.uniqon.kr/auth/signout",{
     method: "DELETE"
   })
+  const jsonResponse = await response.json();
+  
   localStorage.setItem("uniQonSignedIn", jsonResponse.signedIn);
 
   if (localStorage.getItem("uniQonSignedIn") === true){
     window.location.replace("{{ site.baseurl }}assets\js\signout.js");
   }
+  console.log(jsonResponse);
 }
