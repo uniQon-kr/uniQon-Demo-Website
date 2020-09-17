de162f27b1380966b19c1b2edea6c891d323285f
Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
2020-09-17 09:30:39 -0500

    Fix Async

--- assets/js/loginCheck.js
+++ assets/js/loginCheck.js
@@ -1,19 +1,23 @@
-if (localStorage.getItem("uniQonSignedIn") === null) {
-    // call GET https://api.uniqon.kr/auth to check whether user is logged in or not
-    const response = await fetch("https://api.uniqon.kr/auth", {
-        method: "GET"
-    });
-    const jsonResponse = await response.json();
+async function loginCheck() {
+    if (localStorage.getItem("uniQonSignedIn") === null) {
+        // call GET https://api.uniqon.kr/auth to check whether user is logged in or not
+        const response = await fetch("https://api.uniqon.kr/auth", {
+            method: "GET"
+        });
+        const jsonResponse = await response.json();
 
-    // Save signedIn value to local Storage
-    localStorage.setItem("uniQonSignedIn", jsonResponse.signedIn);
+        // Save signedIn value to local Storage
+        localStorage.setItem("uniQonSignedIn", jsonResponse.signedIn);
+    }
+
+    // parse localStorage and display button accordingly
+    if (localStorage.getItem("uniQonSignedIn") === true) {
+        document.getElementsByClassName("unauth").style.display = "none";
+        document.getElementsByClassName("auth").style.display = "block";
+    } else {
+        document.getElementsByClassName("unauth").style.display = "none";
+        document.getElementsByClassName("auth").style.display = "block";
+    }
 }
 
-// parse localStorage and display button accordingly
-if (localStorage.getItem("uniQonSignedIn") === true) {
-    document.getElementsByClassName("unauth").style.display = none;
-    document.getElementsByClassName("auth").style.display = block;
-} else {
-    document.getElementsByClassName("unauth").style.display = none;
-    document.getElementsByClassName("auth").style.display = block;
-}
+loginCheck();
