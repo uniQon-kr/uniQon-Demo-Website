a4e58644b03dddc32603fee203a41355f9148c14
Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
2020-09-17 09:00:56 -0500

    Steve Signed In Update

--- assets/js/loginCheck.js
+++ assets/js/loginCheck.js
@@ -1,6 +1,17 @@
 if (localStorage.getItem("uniQonSignedIn") === null) {
-    // If not setted, call GET https://api.uniqon.kr/auth to check whether user is logged in or not
+    // call GET https://api.uniqon.kr/auth to check whether user is logged in or not
+    const response = await fetch("https://api.uniqon.kr/auth", {
+        method: "GET"
+    });
+    const jsonResponse = await response.json();
+
     // Save signedIn value to local Storage
+    localStorage.setItem("uniQonSignedIn", jsonResponse.signedIn);
 }
-// TODO: parse localStorage and 
-// display button accordingly
+
+// parse localStorage and display button accordingly
+if (localStorage.getItem("uniQonSignedIn") === true) {
+    
+} else {
+
+}
