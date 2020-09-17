72b7f61a1b38b2324a18f9fc435b62a784137361
hse2527 <55567478+hse2527@users.noreply.github.com>
2020-09-18 01:34:22 +0900

    signout url

--- assets/js/signout.js
+++ assets/js/signout.js
@@ -1,6 +1,6 @@
 
 async function signOutFunc(){
-  const response = await fetch("http://uniqon.kr/signout/",{
+  const response = await fetch("http://api.uniqon.kr/signout",{
     method: "DELETE"
   })
   localStorage.setItem("uniQonSignedIn", jsonResponse.signedIn);
