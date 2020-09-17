d3201891d333c2dbe188423b052a5bf7d5a934b1
Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
2020-09-17 08:44:26 -0500

    Steve Header TODOs

---
+++ assets/js/loginCheck.js
@@ -0,0 +1,5 @@
+// TODO: Check whether signedIn value in localStorage has been set
+    // If not setted, call GET https://api.uniqon.kr/auth to check whether user is logged in or not
+        // Save signedIn value to local Storage
+// TODO: parse localStorage and 
+// display button accordingly
