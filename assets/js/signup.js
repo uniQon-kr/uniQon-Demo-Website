6c37eede2d738b262a2b6a5b77332509351f48d9
hse2527 <55567478+hse2527@users.noreply.github.com>
2020-09-18 22:57:41 +0900

    sign-up

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -20,6 +20,7 @@ async function tsCheck(){
 }
 
 async function signUpFunc() {
+
   const tnc = document.getElementById('terms').checked;
   if(tnc === false) {
     document.getElementById("invalid").style.display = "none";
@@ -29,8 +30,42 @@ async function signUpFunc() {
     return;
   }
 
-  const username = document.getElementById('username').value;
+  
+	const username = document.getElementById('username').value;
   const firstName = document.getElementById('firstName').value;
   const lastName = document.getElementById('lastName').value;
   const password = document.getElementById('password').value;
+  const email = document.getElementById('email').value;
+  const nickname = document.getElementById('nickname').value;
+
+	if(username === "" || firstName === "" || lastName === "" || password === "" || email === "" || nickname === "") {
+		// if not properly filled in, show error message
+		document.getElementById("invalid").style.display = "block";
+    document.getElementById("not-match").style.display = "none";
+  }
+  else {
+    // if properly filled in, send the username and password inputs to the auth API
+    const userInput = {
+      id : username,
+      firstName: firstName,
+      lastName: lastName,
+      password : password,
+      email: email,
+      nickname: nickname
+    };
+    const response = await fetch('https://api.uniqon.kr/auth/signup', {
+      method: 'POST',
+      body: JSON.stringify(userInput),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    // Check Response Code and prompt error message if needed
+    if(response.status === 400) {
+      document.getElementById("invalid").style.display = "block";
+      return;
+    } 
+    history.back();
+  }
 }
