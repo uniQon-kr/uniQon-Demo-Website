12501cd9efae6cbc8658cc7cb46097dd0d11745e
Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
2020-09-19 06:47:26 -0500

    Get MyInfo

--- assets/js/mypage.js
+++ assets/js/mypage.js
@@ -11,16 +11,20 @@ async function getMyInfo() {
     if(response.ok) {
         const jsonResponse = await response.json();
         // Common
+        const { nickname, email } = jsonResponse;
         document.getElementById("title").innerHTML = "Hi! " + jsonResponse.firstName + " " + jsonResponse.lastName;
-        document.getElementById("nickname").value = jsonResponse.nickname;
-        document.getElementById("email").value = jsonResponse.email;
+        document.getElementById("nickname").value = nickname;
+        document.getElementById("email").value = email;
         // Mentor
         if(jsonResponse.type.includes("mentor")) {
             document.getElementById("mentor").style.display = "block";
+            document.getElementById("balance").innerHTML = jsonResponse.remainingBalance;
         }
         // Mentee
         if(jsonResponse.type.includes("mentee")) {
             document.getElementById("mentee").style.display = "block";
+            document.getElementById("usedApp").innerHTML = jsonResponse.usedApp;
+            document.getElementById("remainingApp").innerHTML = jsonResponse.remainingApp;
         }
     } else if(response.status === 401 || response.status == 403) { // Unauthorized OR Forbidden
         localStorage.setItem("uniQonSignedIn", false);
