42c2e2795e01b9ef2dbdf90c1e5f8b7536a019eb
hse2527 <55567478+hse2527@users.noreply.github.com>
2020-09-23 20:31:12 +0900

    list.js

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,9 +1,9 @@
-let listRem,index,from,to,maxPage,length;
+let page = 1;
 
 async function list() {
-  const page = Number(document.getElementById("page").innerHTML);
+  console.log(page);
   const response = await fetch('https://api.uniqon.kr/document/application/list', {
-      url: 'https://api.uniqon.kr/document/application/list?from=' + page*15 + '&to=' + page*15-14,
+      url: 'https://api.uniqon.kr/document/application/list?from=' + page*15-14 + '&to=' + page*15+1,
       credentials: 'include',
       method: 'GET', 
       headers: {
@@ -12,32 +12,33 @@ async function list() {
   });
   if(response.ok) {
     jsonResponse = await response.json();
-    length = jsonResponse.appList.length;
-    maxPage = length/15;
-  }
-  
-  if (page===1){
-    document.getElementById('previous').style.display = "none";
-  } else if (page===maxPage){
-    document.getElementById('next').style.display = "none";
-  } else {
-    document.getElementById('previous').style.display = "initial";
-    document.getElementById('next').style.display = "initial";
-  }
-  
-  for (i = 0; i < 15; i++) {
-    document.getElementById("mentor-wrapper-" + i).style.display = "none";
-    document.getElementById("college-name-" + i).src = "";
-  }
-  
-  if (length > 15) {
-    for (i = 0; i < 15; i++) { 
-      let jsonObj = jsonResponse.appList[i];
-      
-      document.getElementById("mentor-wrapper-" + i).style.display = "grid";
-      document.getElementById("mentor-school-" + i).innerHTML = jsonObj.collegeName + "(" + jsonObj.expectedGrad + ")";
-      document.getElementById("mentor-name-" + i).innerHTML = jsonObj.mentorID;
-      document.getElementById("college-name-" + i).src = "/assets/school-logo/" + jsonObj.collegeName.replace(/ /g,'-') + ".png";
+    const length = jsonResponse.appList.length;
+    let nextPage = false;
+    if(jsonResponse.appList[15]!=null){nextPage = true;}
+    
+    if (page===1){
+      document.getElementById('previous').style.display = "none";
+    } else if (!nextPage){
+      document.getElementById('next').style.display = "none";
+    } else {
+      document.getElementById('previous').style.display = "initial";
+      document.getElementById('next').style.display = "initial";
+    }
+    
+    for (i = 0; i < 15; i++) {
+      document.getElementById("mentor-wrapper-" + i).style.display = "none";
+      document.getElementById("college-name-" + i).src = "";
+    }
+    
+    if (length > 15) {
+      for (i = 0; i < 15; i++) { 
+        let jsonObj = jsonResponse.appList[i];
+        
+        document.getElementById("mentor-wrapper-" + i).style.display = "grid";
+        document.getElementById("mentor-school-" + i).innerHTML = jsonObj.collegeName + "(" + jsonObj.expectedGrad + ")";
+        document.getElementById("mentor-name-" + i).innerHTML = jsonObj.mentorID;
+        document.getElementById("college-name-" + i).src = "/assets/school-logo/" + jsonObj.collegeName.replace(/ /g,'-') + ".png";
+      }
     }
   }else{
     for (i = 0; i < length; i++) {
