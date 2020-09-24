7ddac95e9963d75814c0e58e3ea084435f926c25
hse2527 <55567478+hse2527@users.noreply.github.com>
2020-09-25 01:45:40 +0900

    conflict fixed

--- assets/js/mentor-form.js
+++ assets/js/mentor-form.js
@@ -1,20 +1,61 @@
 // boolean variable to keep track whether the form has been updated after last draft save
 let isUpdated = false;
 let updateTimer = null;
+let hooksCount = 1;
+let sat2Count = 1;
+let apCount = 1;
+let honorsCount = 1;
+let activitiesCount = 1;
+let essayCount = 1;
+let additionalCount = 1;
 
 async function addMore(type) {
-  if(type === ){
-    
-  } else if(type === ){
-
-  } else if(type === ){
-
-  } else if(type === ){
-
-  } else if(type === ){
-
-  } else if(type === ){
-
+  //add more textfields to its type
+  if(type === "hooks"){
+    hooksCount++;
+    document.getElementById("hooks-AM").innerHTML += "<input id = 'form-hooks-" + hooksCount +"' type = 'text'/>";
+  } else if(type === "sat2"){
+    sat2Count++;
+    document.getElementById("sat2-AM").innerHTML +=
+    "<input id = 'form-sat2-subject-" + 
+    sat2Count + "' type = 'text' placeholder= 'Subject'/><input id = 'form-sat2-score-" + 
+    sat2Count + "' type = 'text' placeholder= 'Score'/>";
+  } else if(type === "ap"){
+    apCount++;
+    document.getElementById("ap-AM").innerHTML +="<input id = 'form-ap-subject-"+
+    apCount+"' type = 'text' placeholder='Subject'/><input id = 'form-ap-score-"+ 
+    apCount+"' type = 'text' placeholder='Score'/>";
+  } else if(type === "honors"){
+    honorsCount++;
+    document.getElementById("honors-AM").innerHTML +="<div class = 'entryBox'><p class = 'content-title'>Title</p><input id = 'form-honors-title-"+
+      honorsCount+"' type = 'text'/><p class = 'content-title'>Grade Level</p><div class = 'checkboxes'><input type='checkbox' id='h"+
+      honorsCount+"-9' value='9'><label>9</label><input type='checkbox' id='h"+
+      honorsCount+"-10' value='10'><label>10</label><input type='checkbox' id='h"+
+      honorsCount+"-11' value='11'><label>11</label><input type='checkbox' id='h"+
+      honorsCount+"-12' value='12'><label>12</label><input type='checkbox' id='h"+
+      honorsCount+"-13' value='13'><label>Post-Graduate</label></div><p class = 'content-title'>Level of Recognition</p><input id = 'form-honors-lvl-"+
+      honorsCount+"' type = 'text'/></div>";
+  } else if(type === "activities"){
+    activitiesCount++;
+    document.getElementById("activities-AM").innerHTML +="<div class = 'entryBox'><p class = 'content-title'>Title</p><input id = 'form-activities-type-"+
+      activitiesCount+"' type = 'text'/><p class = 'content-title'>Position</p><input id = 'form-activities-position-"+
+      activitiesCount+"' type = 'text'/><p class = 'content-title'>Organization</p><input id = 'form-activities-organization-"+
+      activitiesCount+"' type = 'text'/><p class = 'content-title'>Description</p><input id = 'form-activities-description-"+
+      activitiesCount+"' type = 'text'/><p class = 'content-title'>Participation Grade</p><div class = 'checkboxes'><input type='checkbox' id='a"+
+      activitiesCount+"-9' value='9'><label>9</label><input type='checkbox' id='a"+
+      activitiesCount+"-10' value='10'><label>10</label><input type='checkbox' id='a"+
+      activitiesCount+"-11' value='11'><label>11</label><input type='checkbox' id='a"+
+      activitiesCount+"-12' value='12'><label>12</label><input type='checkbox' id='a"+
+      activitiesCount+"-13' value='13'><label>Post-Graduate</label></div><p class = 'content-title'>Time of Participation</p><input id = 'form-activities-time-"+
+      activitiesCount+"' type = 'text'/></div>";
+      activitiesCount++;
+  } else if(type === "essay"){
+    essayCount++;
+    document.getElementById("essay-AM").innerHTML +="<div class = 'entryBox'><input id = 'form-essay-prompt-"+
+      essayCount+"' type = 'text' placeholder='Prompt'/><textarea class = 'essay' id= essay-"+
+      essayCount+"'></textarea></div>";
+  }else if(type === "additional"){
+    document.getElementById("additional-AM").innerHTML +="";
   }
 }
 
