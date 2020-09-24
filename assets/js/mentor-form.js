5b0221ee3f0cc116038fbbdf59f47dff52cb1faa
Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
2020-09-24 09:59:33 -0500

    JavaScript TODOs

---
+++ assets/js/mentor-form.js
@@ -0,0 +1,22 @@
+// boolean variable to keep track whether the form has been updated after last draft save
+isUpdated = false;
+
+async function loadDraft() {
+    // TODO: function to load draft when the page first loaded
+}
+
+async function submit() {
+    // TODO: submit the form as final version
+}
+
+async function saveDraft() {
+    // TODO: save as draft
+}
+
+function formUpdated() {
+    // TODO: change isUpdated to true (on key press for all text field)
+}
+
+loadDraft();
+
+// start timer, save every one min
